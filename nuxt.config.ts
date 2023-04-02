d9338d662975d9db027982f5e9096e29b42af535
Nikolay Dorofeev <dorich2000@gmail.com>
2023-04-02 12:23:35 +0400

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,40 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  extends: ['@d0rich/nuxt-design-system'],
+  app: {
+    pageTransition: {
+      name: 'section',
+      mode: 'out-in'
+    }
+  },
+  css: ['assets/css/theme.css'],
+  imports: {
+    dirs: ['composables/*/index.{ts,js,mjs,mts}']
+  },
+  runtimeConfig: {
+    public: {
+      isDev: process.env.NODE_ENV === 'development',
+      isProd: process.env.NODE_ENV === 'production'
+    }
+  },
+  modules: ['@nuxt/content'],
+  nitro: {
+    prerender: {
+      routes: [
+        '/api/resume/Nikolay_Dorofeev-Software Engineer.pdf',
+        '/api/resume/Nikolay_Dorofeev-Node.js Developer.pdf',
+        '/api/resume/Nikolay_Dorofeev-Vue Frontend Developer.pdf',
+        '/api/resume/Nikolay_Dorofeev-System Analyst.pdf'
+      ]
+    }
+  },
+  content: {
+    markdown: {
+      rehypePlugins: {
+        'rehype-external-links': {
+          target: '_blank'
+        }
+      }
+    }
+  }
+})
