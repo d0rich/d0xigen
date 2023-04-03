df62432c99fdd2c11c452a043fce92d15580dc7f
Nikolay Dorofeev <dorich2000@gmail.com>
2023-04-03 23:45:30 +0400

    Remove tailwind typography classes

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -25,24 +25,8 @@ tableOfContents.value = doc.value?.body?.toc ?? null
         :value="doc"
         tag="article"
       >
-        <ContentRendererMarkdown :value="doc" class="d-article" />
+        <ContentRendererMarkdown :value="doc" />
       </ContentRenderer>
     </NuxtLayout>
   </div>
 </template>
-
-<style>
-.d-article {
-  @apply prose dark:prose-invert font-sans
-         md:prose-lg
-         mx-auto sm:px-0
-         prose-pre:bg-inherit
-         dark:prose-headings:text-cyan-400 dark:prose-a:text-inherit
-         dark:prose-p:first-letter:bg-cyan-800
-         dark:prose-blockquote:border-cyan-400
-         dark:marker:text-cyan-400;
-}
-.d-article blockquote p::before {
-  content: none;
-}
-</style>
