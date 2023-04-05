20acc689479c08cb5604f205a57eb4037a3f49f6
Nikolay Dorofeev <dorich2000@gmail.com>
2023-04-05 18:47:26 +0400

    Memoize og image

--- server/routes/og/image.jpg.ts
+++ server/routes/og/image.jpg.ts
@@ -1,10 +1,19 @@
 import nodeHtmlToImage from 'node-html-to-image'
 
+let cachedBuffer: Buffer | undefined
+
 export default defineEventHandler(async (event) => {
+  if (cachedBuffer) {
+    return cachedBuffer
+  }
   const html = await $fetch('/og/image')
-  return nodeHtmlToImage({
+  const image = await nodeHtmlToImage({
     html: html,
     waitUntil: 'networkidle0',
     type: 'jpeg'
   })
+  if (image instanceof Buffer) {
+    cachedBuffer = image
+  }
+  return image
 })
