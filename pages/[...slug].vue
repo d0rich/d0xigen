a6572bad04827cff051926ecb87eb0f6a5cd334c
Nikolay Dorofeev <dorich2000@gmail.com>
2023-04-05 15:29:06 +0400

    Fix hash links top scroll

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -25,13 +25,15 @@ tableOfContents.value = doc.value?.body?.toc ?? null
       :description="doc.description"
     />
     <NuxtLayout>
-      <ContentRenderer
-        v-if="doc && doc._type === 'markdown'"
-        :value="doc"
-        tag="article"
-      >
-        <ContentRendererMarkdown :value="doc" />
+      <ContentRenderer v-if="doc && doc._type === 'markdown'" :value="doc">
+        <ContentRendererMarkdown tag="article" class="d-article" :value="doc" />
       </ContentRenderer>
     </NuxtLayout>
   </div>
 </template>
+
+<style>
+.d-article :is(h1, h2, h3, h4, h5, h6) {
+  @apply !pt-20 !-mt-20;
+}
+</style>
