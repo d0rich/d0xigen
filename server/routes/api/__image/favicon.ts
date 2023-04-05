9cca75b31245a4437eb417192f316cfa71acfe02
Nikolay Dorofeev <dorich2000@gmail.com>
2023-04-05 19:01:35 +0400

    Generate favicon in html

---
+++ server/routes/api/__image/favicon.ts
@@ -0,0 +1,9 @@
+export default defineEventHandler(async (event) => {
+  const appConfig = useAppConfig()
+  const storage = useStorage('assets:server')
+  const template = await storage.getItem('templates:favicon.html')
+  const background = await storage.getItem('templates:favicon.svg')
+  return template?.toString()
+    .replace('{{background}}', background?.toString() ?? '')
+    .replace('{{signature}}', appConfig.d0xigen.title[0])
+})
