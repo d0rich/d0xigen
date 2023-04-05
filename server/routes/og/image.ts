8ea2d8d503e4313474ea931d9a02b75fca5742ea
Nikolay Dorofeev <dorich2000@gmail.com>
2023-04-05 18:33:08 +0400

    Template for og-image

--- server/routes/og/image.ts
+++ server/routes/og/image.ts
@@ -1,7 +1,12 @@
 
 
 export default defineEventHandler(async (event) => {
+  const appConfig = useAppConfig()
   const storage = useStorage('assets:server')
   const template = await storage.getItem('templates:og-image.html')
-  return template
+  const background = await storage.getItem('templates:background.svg')
+  return template?.toString()
+    .replace('{{background}}', background?.toString() ?? '')
+    .replace('{{title}}', appConfig.d0xigen.title)
+    .replace('{{description}}', appConfig.d0xigen.description)
 })
