8ffceb324d556dc572a95da7375a15bc7dc484c7
d0rich <d0rich@users.noreply.github.com>
2023-04-06 15:09:29 +0000

    Automatic deploy: Suppress mermaid errors 9564851d797154a72a4815ab3793634b382d024b

---
+++ _nuxt/Markdown.d4e714bd.js
@@ -0,0 +1,1 @@
+import r from"./ContentSlot.5513e418.js";import{a as o,J as u,j as f,x as c}from"./entry.20e40fb8.js";const i=o({name:"Markdown",extends:r,setup(t){const{parent:e}=c(),{between:n,default:a}=u(),s=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:s,between:n,parent:e}}});export{i as default};
