8b90358364539ec39250f0ad20ae57960fc71579
d0rich <d0rich@users.noreply.github.com>
2023-04-06 15:38:49 +0000

    Automatic deploy: Remove mermaid timer, more test pages 823ae1f5bb5ccf83074f1335e21e1d56cca84d15

--- _nuxt/Markdown.8be7d2c9.js
+++
@@ -1,1 +0,0 @@
-import r from"./ContentSlot.1eb95f37.js";import{a as o,J as u,j as f,y as c}from"./entry.22f210a6.js";const i=o({name:"Markdown",extends:r,setup(t){const{parent:e}=c(),{between:n,default:a}=u(),s=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:s,between:n,parent:e}}});export{i as default};
