c14307f248c9810023ad54a4844047a02b02ed4d
d0rich <d0rich@users.noreply.github.com>
2023-04-06 11:53:35 +0000

    Automatic deploy: Update design system af049e1aca80f2581349caab10fe0f3e105edcef

---
+++ _nuxt/ProseLi.b916a76c.js
@@ -0,0 +1,1 @@
+/* empty css                    */import{_ as o}from"./_plugin-vue_export-helper.c27b6911.js";import{o as r,b as t,q as n}from"./entry.56bf801b.js";const s={};function a(e,c){return r(),t("li",null,[n(e.$slots,"default",{},void 0,!0)])}const f=o(s,[["render",a],["__scopeId","data-v-8d545697"]]);export{f as default};
